bfd7fa68892269ab6a39e06d2fb7b292e95c7da5
paj <per.anders.jorgensen@gmail.com>
2018-09-27 15:58:57 +0200

    Fixed registry error and updated MCSServices

--- api-implementation/registry.js
+++ api-implementation/registry.js
@@ -1,13 +1,13 @@
 module.exports = {
   components: {
     // PizzaBot
-    'AgeChecker': require('./pizza/age_checker')
+    'AgeChecker': require('./pizza/age_checker'),
 	  
     // FinancialBot
     'BalanceRetrieval': require('./banking/balance_retrieval'),
     'TransactionsRetrieval': require('./banking/transactions_retrieval'),
     'TrackSpending': require('./banking/track_spending'),
-    'Payments': require('./banking/payments'),
+    'Payments': require('./banking/payments')
 	  
 	  	  
   }
