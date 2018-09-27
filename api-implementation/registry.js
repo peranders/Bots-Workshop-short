9b62aa5e7e43d2896a4318d08751bc83a9d2be9c
paj <per.anders.jorgensen@gmail.com>
2018-09-27 15:21:56 +0200

    Added banking sample

--- api-implementation/registry.js
+++ api-implementation/registry.js
@@ -2,5 +2,13 @@ module.exports = {
   components: {
     // PizzaBot
     'AgeChecker': require('./pizza/age_checker')
+	  
+    // FinancialBot
+    'BalanceRetrieval': require('./banking/balance_retrieval'),
+    'TransactionsRetrieval': require('./banking/transactions_retrieval'),
+    'TrackSpending': require('./banking/track_spending'),
+    'Payments': require('./banking/payments'),
+	  
+	  	  
   }
 };
